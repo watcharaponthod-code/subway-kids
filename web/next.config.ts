19919228e87febd0881cf9473294f17e84058272
Watcharapon-T <watcharapon.thod@gmail.com>
2026-05-04 15:41:27 +0700

    Update web/next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "standalone",   // required for Docker multi-stage build
+};
 
 export default nextConfig;
